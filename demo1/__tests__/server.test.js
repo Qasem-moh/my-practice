000a375baba92dcea3be18ffccf2b55a8d8bd0f7
qasem-moh <2171246@s.aou.edu.jo>
2021-08-21 02:29:35 +0300

    demo1

--- demo1/__tests__/server.test.js
+++ demo1/__tests__/server.test.js
@@ -1,23 +1,26 @@
 'use sttrict';
 
-const server=require('../server.js')
-const supertest=require('supertest')
-const request=supertest(server.app)
+const server = require('../server.js')
+const supertest = require('supertest')
+const request = supertest(server.app)
 describe('API Server', () => {
 
     it('handles invalid request not found', async () => {
-        const response =await request.get('/asd');
+        const response = await request.get('/asd');
         expect(response.status).toEqual(404)
     })
-
-    it('handles my internail server errors', () => {
-
+    it('handles my internail server errors', async () => {
+        const response = await request.post('/bad');
+        expect(response.status).toEqual(500)
+    })
+    it('handles my internail server errors', async () => {
+        const response = await request.get('/data');
+        expect(response.status).toEqual(200)
+        expect(typeof response.body).toEqual('object')
     })
 
-    it('get data from /data', () => {
-
-    })
-    it('/route works', () => {
-
+    it('/route works', async () => {
+        const response = await request.get('/')
+        expect(response.text).toEqual("Hello in Home Page")
     })
 })
